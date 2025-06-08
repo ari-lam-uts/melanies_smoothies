ac24a8bdfa50c8895f41fa2360057f9b13751d3b
ari-lam-uts <ari.lam@alumni.uts.edu.au>
2025-06-08 12:53:39 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,8 +18,7 @@ cnx = st.connection('snowflake')
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 
-pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)                                  
+pd_df = my_dataframe.to_pandas()                              
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:', my_dataframe, max_selections=5
